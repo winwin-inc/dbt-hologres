7b28418be82846a68ebd6f3d7d1b8040344d049b
lizhongshan <lizhongshan@brandct.com>
2025-11-02 17:12:07 +0800

    fix: 去除表的长度限制

--- src/dbt/adapters/hologres/relation.py
+++ src/dbt/adapters/hologres/relation.py
@@ -1,10 +1,12 @@
 from dataclasses import dataclass, field
 
-from dbt.adapters.base.relation import BaseRelation
-from dbt.adapters.postgres import PostgresRelation
+ from dbt.adapters.postgres import PostgresRelation
 from dbt.adapters.contracts.relation import Policy
 
 
 @dataclass(frozen=True, eq=False, repr=False)
 class HologresRelation(PostgresRelation):
     include_policy: Policy = field(default_factory=lambda: Policy(database=False))
+
+    def relation_max_name_length(self):
+        return 1024
