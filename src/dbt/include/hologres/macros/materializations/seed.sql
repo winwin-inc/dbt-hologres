4f16b0d8f75ffbc5f0ac3e7e481a4162be3a80db
lizhongshan <lizhongshan@brandct.com>
2025-11-22 15:40:46 +0800

    fix: INSERT in ddl transaction is not supported now

--- src/dbt/include/hologres/macros/materializations/seed.sql
+++ src/dbt/include/hologres/macros/materializations/seed.sql
@@ -16,10 +16,6 @@
 
   {{ run_hooks(pre_hooks, inside_transaction=False) }}
 
-  -- `BEGIN` happens here:
-  {{ run_hooks(pre_hooks, inside_transaction=True) }}
-
-  -- build model
   {% set create_table_sql = "" %}
   {% if exists_as_view %}
     {{ exceptions.raise_compiler_error("Cannot seed to '{}', it is a view".format(old_relation)) }}
@@ -29,12 +25,21 @@
     {% set create_table_sql = create_csv_table(model, agate_table) %}
   {% endif %}
 
+  -- Execute DDL in its own transaction and commit it immediately
+  {% call statement('create_table') -%}
+    {{ create_table_sql }}
+  {%- endcall %}
+  {{ adapter.commit() }}
+
+  -- Now begin a fresh transaction for DML operations
+  {{ run_hooks(pre_hooks, inside_transaction=True) }}
+
   {% set code = 'CREATE' if full_refresh_mode else 'INSERT' %}
   {% set rows_affected = (agate_table.rows | length) %}
   {% set sql = load_csv_rows(model, agate_table) %}
 
   {% call noop_statement('main', code ~ ' ' ~ rows_affected, code, rows_affected) %}
-    {{ get_csv_sql(create_table_sql, sql) }};
+    {{ sql }}
   {% endcall %}
 
   {% set target_relation = this.incorporate(type='table') %}
@@ -50,12 +55,6 @@
 
   {{ run_hooks(post_hooks, inside_transaction=True) }}
 
-  {% call statement('end') -%}
-      end;
-  {%- endcall %}
-
-  
-
   -- `COMMIT` happens here
   {{ adapter.commit() }}
 
@@ -64,3 +63,79 @@
   {{ return({'relations': [target_relation]}) }}
 
 {% endmaterialization %}
+
+
+{% macro hologres__get_csv_sql(create_or_truncate_sql, insert_sql) %}
+  -- For Hologres, DDL and INSERT must be in separate transactions
+  -- This macro only returns the DDL part; INSERT is handled separately
+  {{ create_or_truncate_sql }}
+{% endmacro %}
+
+
+{% macro hologres__create_csv_table(model, agate_table) %}
+  {%- set column_override = model['config'].get('column_types', {}) -%}
+  {%- set quote_seed_column = model['config'].get('quote_columns', None) -%}
+
+  {% set sql %}
+    create table {{ this.render() }} (
+        {%- for col_name in agate_table.column_names -%}
+            {%- set inferred_type = adapter.convert_type(agate_table, loop.index0) -%}
+            {%- set type = column_override.get(col_name, inferred_type) -%}
+            {%- set column_name = (col_name | string) -%}
+            {{ adapter.quote_seed_column(column_name, quote_seed_column) }} {{ type }} {%- if not loop.last -%}, {%- endif -%}
+        {%- endfor -%}
+    )
+  {% endset %}
+
+  {{ return(sql) }}
+{% endmacro %}
+
+
+{% macro hologres__reset_csv_table(model, full_refresh, old_relation, agate_table) %}
+  {% set sql = "" %}
+  {% if full_refresh %}
+    -- Drop and recreate the table
+    {% set sql = "drop table if exists " ~ old_relation ~ "; " ~ create_csv_table(model, agate_table) %}
+  {% else %}
+    -- Truncate the existing table
+    {% set sql = "truncate table " ~ old_relation %}
+  {% endif %}
+
+  {{ return(sql) }}
+{% endmacro %}
+
+
+{% macro hologres__load_csv_rows(model, agate_table) %}
+  {% set batch_size = get_batch_size() %}
+
+  {% set cols_sql = get_seed_column_quoted_csv(model, agate_table.column_names) %}
+  {% set bindings = [] %}
+  {% set statements = [] %}
+
+  {% for chunk in agate_table.rows | batch(batch_size) %}
+      {% set bindings = [] %}
+
+      {% for row in chunk %}
+          {% do bindings.extend(row) %}
+      {% endfor %}
+
+      {% set sql %}
+          insert into {{ this.render() }} ({{ cols_sql }}) values
+          {% for row in chunk -%}
+              ({%- for column in agate_table.column_names -%}
+                  {{ get_binding_char() }}
+                  {%- if not loop.last%},{%- endif %}
+              {%- endfor -%})
+              {%- if not loop.last%},{%- endif %}
+          {%- endfor %}
+      {% endset %}
+
+      {% do adapter.add_query(sql, bindings=bindings, abridge_sql_log=True) %}
+
+      {% if loop.index0 == 0 %}
+          {% do statements.append(sql) %}
+      {% endif %}
+  {% endfor %}
+
+  {{ return(statements[0]) }}
+{% endmacro %}
